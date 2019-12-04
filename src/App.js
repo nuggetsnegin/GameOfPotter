db32064be0c2ce7b56b6767edb52f97b9c41efaa
Negin <nuggetnegin@gmail.com>
2019-12-04 09:26:03 -0500

    setup axios, sass and harry potter api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './styles/App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      HPCharacters: [],
+      GoTCharacters: [],
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      method: "get",
+      url: "https://www.potterapi.com/v1/characters",
+      dataResponse: "json",
+      params: {
+        key: "$2a$10$QTTp9tiCR8CNBsj3iA5IR.jJhfdT2FKcAnZsP2gYYGaI27KsGEVwy",
+      },
+    }).then(response => {
+      console.log(response.data);
+
+      this.setState({
+        HPCharacters: response.data,
+      });
+    });
+  }
+
+  /*do got api https://anapioficeandfire.com/*/
+
+  render() {
+    return (
+      <div className='wrapper'>
+        <h1>Game of Potter</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
