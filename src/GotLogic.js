ce9ea6ce0b2bf5d31391371b58e84772e08f2a7c
keil <keilwmartin@gmail.com>
2019-12-05 14:27:28 -0500

    started the logic for the comparrison of charecters

---
+++ src/GotLogic.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class GotLogic extends Component {
+    constructor() {
+        super();
+    }
+
+    getCharScore = (e, choiceOfCharecter) => {
+
+        const originalArray = [...this.state.gotCharacters]
+
+    Const originalArray = () => {
+    if (house === "House Stark") {
+        value = "100";
+    } else if (house === "House Tyrell") {
+        value = "75";
+    } else if (house === "House Greyjoy") {
+        value = "50";
+    } else if (house === "House Greyjoy") {
+        value = "50";
+    } else {
+        value = "25";
+    }
+
+
+
+
+    // ". notation" here because its on the object //
+    const  "defindTERM"= originalArray.filter((charecter) => {
+        return charecter."definedATTRIBUE" === true;
+    // when true apply multiplyer (eg. return vale )
+});
+
+
+// // ". notation" here because its on the object //
+//         const "defindTERM2" = "defindTERM".filter((charecterTwo) => {
+//             return charecterTwo.deathEater === true;
+//                 // when true apply multiplyer (eg. return vale )
+//             console
+// });
+
+console.log("definedTERM2")
+
+this.setState({
+    finalCharSelection: "defindTERM2"
+})
+}
+
+
+
+
+
