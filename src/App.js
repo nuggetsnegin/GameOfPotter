1d1f7bb2831040b4bb090af71a69e449666a39f4
Negin <nuggetnegin@gmail.com>
2019-12-06 10:54:08 -0500

    I JUST WANT TO MAKE A PULL REQUEST

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <div className='wrapper'>
-        <h1>Game of Potter</h1>
+        <h1>Game of Potter HELLO?</h1>
         <GetGotCharacters/>
         <GetHpCharacters/>
         <RandomizeCharacters gotCharactersArray={this.state.gotCharacters}/>
