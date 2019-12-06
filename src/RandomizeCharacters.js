06e1d7ad9c779ef7f29fafb6cdbbfe3b7eab6532
Negin <nuggetnegin@gmail.com>
2019-12-06 12:01:28 -0500

    fixed infinite loop in api

--- src/RandomizeCharacters.js
+++ src/RandomizeCharacters.js
@@ -1,29 +1,33 @@
 import React, { Component } from "react";
 
 const RandomizeCharacters = (props) => {
+// gotCharactersArray   
+console.log(props.gotCharactersArray);
 
-    // generate a random number between 0 and 3
-    const randomNumberHouse = Math.floor(Math.random() * 3);
-    console.log(randomNumberHouse);
-    // use the random number to pick a house from the 4 houses
-    const randomHouse = props.gotCharactersArray[randomNumberHouse];
-    console.log(randomHouse);
-    // const arrayLength = randomHouse.length;
-    // console.log(arrayLength);
+// let randomshit = Math.floor(Math.random() * Math.floor(props.le))
 
-    // const arrayLength = randomHouse.length was coming back undefined???
-    // used if statement to get
-    if (randomHouse !== undefined) {
-        // get the length of the house array chosen above
-        const arrayLength = randomHouse.length;
-        console.log(arrayLength);
-        // generate a random number between 0 and that length
-        const randomNumberCharacter = Math.floor(Math.random() * arrayLength);
-        console.log(randomNumberCharacter);
-        // use that random number as an index to pick a character from the house
-        const randomCharacter = randomHouse[randomNumberCharacter];
-        console.log(randomCharacter);
-    }
+    // // generate a random number between 0 and 3
+    // const randomNumberHouse = Math.floor(Math.random() * 3);
+    // console.log(randomNumberHouse);
+    // // use the random number to pick a house from the 4 houses
+    // const randomHouse = props.gotCharactersArray[randomNumberHouse];
+    // console.log(randomHouse);
+    // // const arrayLength = randomHouse.length;
+    // // console.log(arrayLength);
+
+    // // const arrayLength = randomHouse.length was coming back undefined???
+    // // used if statement to get
+ 
+    //     // get the length of the house array chosen above
+    //     const arrayLength = randomHouse.length;
+    //     console.log(arrayLength);
+    //     // generate a random number between 0 and that length
+    //     const randomNumberCharacter = Math.floor(Math.random() * arrayLength);
+    //     console.log(randomNumberCharacter);
+    //     // use that random number as an index to pick a character from the house
+    //     const randomCharacter = randomHouse[randomNumberCharacter];
+        // console.log(randomCharacter);
+    
 
     return(
         <div>
