c82f0f2b05ac392e11dc0faa425423d9e7f91e29
Negin <nuggetnegin@gmail.com>
2019-12-09 21:41:40 -0500

    Added intro text and fixed button styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React, { Component } from "react";
-import "./styles/App.css";
-import GetGotCharacters from "./GetGotCharacters";
-import GetHpCharacters from "./GetHpCharacters";
-import Outing from './Outing.js';
-import StatsMatch from './StatsMatch.js';
+import React, { Component } from 'react'
+import './styles/App.css'
+import GetGotCharacters from './GetGotCharacters'
+import GetHpCharacters from './GetHpCharacters'
+import Outing from './Outing.js'
+import StatsMatch from './StatsMatch.js'
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
       randomGotCharacter: {},
       randomHpCharacter: {}
-    };
+    }
   }
 
-  render() {
-    console.log(this.state);
+  render () {
+    console.log(this.state)
     return (
       <div className='wrapper'>
         <h1>Game of Potter</h1>
+        <p className='introduction'>
+          Hi there! Have you ever wanted to know which <span>Game of Thrones</span> and <span>Harry Potter </span> 
+          characters could be BFFS? No? Well.. too bad!
+        </p>
         <div className='gridContainer'>
           <GetGotCharacters setAppState={value => this.setState(value)} />
-          <StatsMatch 
+          <StatsMatch
             got={this.state.randomGotCharacter}
             hp={this.state.randomHpCharacter}
           />
@@ -29,8 +33,8 @@ class App extends Component {
           {/* <Outing /> */}
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
