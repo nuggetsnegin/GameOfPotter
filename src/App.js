0a6aeb1018f6b02ffad1be6fa724d682754e036e
Negin <nuggetnegin@gmail.com>
2019-12-10 12:06:14 -0500

    updated favicon and title, changed background image, fixed vh and text sizing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react'
-import './styles/App.css'
-import GetGotCharacters from './GetGotCharacters'
-import GetHpCharacters from './GetHpCharacters'
-import Outing from './Outing.js'
-import StatsMatch from './StatsMatch.js'
+import React, { Component } from "react";
+import "./styles/App.css";
+import GetGotCharacters from "./GetGotCharacters";
+import GetHpCharacters from "./GetHpCharacters";
+import Outing from "./Outing.js";
+import StatsMatch from "./StatsMatch.js";
 
 class App extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       randomGotCharacter: {},
-      randomHpCharacter: {}
-    }
+      randomHpCharacter: {},
+    };
   }
 
-  render () {
+  render() {
     return (
       <div className='app'>
         <div className='wrapper'>
-          <h1>Game of Potter</h1>
-          <div className='gridContainer'>
-          <div className='introduction'><p>
-            Hi there! Have you ever wanted to know which{' '}
-            <span>Game of Thrones</span> and <span>Harry Potter </span>
-            characters could be BFFS? No? Well.. too bad!</p>
-          </div>
+          <header><h1>Game of Potter</h1></header>
+          <main className='gridContainer'>
+            <div className='introduction'>
+              <p>
+                Hi there! Have you ever wanted to know which{" "}
+                <span>Game of Thrones</span> and <span>Harry Potter </span>
+                characters could be BFFS? No? Well.. too bad!
+              </p>
+            </div>
             <GetGotCharacters setAppState={value => this.setState(value)} />
             <StatsMatch
               got={this.state.randomGotCharacter}
@@ -32,15 +34,15 @@ class App extends Component {
             />
             <GetHpCharacters setAppState={value => this.setState(value)} />
             {/* <Outing /> */}
-          </div>
+          </main>
         </div>
         <footer>
-          Made with magic & blood by <span>Negin</span>, <span>Sarah</span>,{' '}
+          Made with magic & blood by <span>Negin</span>, <span>Sarah</span>,{" "}
           <span>Nuno</span> & <span>Keil</span> 2019
         </footer>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/StatsMatch.js
+++ src/StatsMatch.js
@@ -1,70 +1,72 @@
-import React from 'react'
-import HeartAnimation from './HeartAnimation'
+import React from "react";
+import HeartAnimation from "./HeartAnimation";
 
 const calculateCharacterPoints = character => {
-  let totalPoints = 0
+  let totalPoints = 0;
 
-  const house = character.house
-  if (house === 'Gryffindor' || house === 'House Stark') {
-    totalPoints += Math.floor(Math.random() * 10) + 41
+  const house = character.house;
+  if (house === "Gryffindor" || house === "House Stark") {
+    totalPoints += Math.floor(Math.random() * 10) + 41;
   }
-  if (house === 'Hufflepuff' || house === 'House Tyrell') {
-    totalPoints += Math.floor(Math.random() * 10) + 11
+  if (house === "Hufflepuff" || house === "House Tyrell") {
+    totalPoints += Math.floor(Math.random() * 10) + 11;
   }
-  if (house === 'Ravenclaw') {
-    totalPoints += Math.floor(Math.random() * 5) + 10
+  if (house === "Ravenclaw") {
+    totalPoints += Math.floor(Math.random() * 5) + 10;
   }
-  if (house === 'House Greyjoy') {
-    totalPoints -= Math.floor(Math.random() * 5) + 1
+  if (house === "House Greyjoy") {
+    totalPoints -= Math.floor(Math.random() * 5) + 1;
   }
-  if (house === 'House Lannister' || house === 'Slytherin') {
-    totalPoints -= Math.floor(Math.random() * 10) + 11
+  if (house === "House Lannister" || house === "Slytherin") {
+    totalPoints -= Math.floor(Math.random() * 10) + 11;
   }
 
-  const age = character.age
-  if (age % 2 === 0) totalPoints += 10
-  else totalPoints -= 5
+  const age = character.age;
+  if (age % 2 === 0) totalPoints += 10;
+  else totalPoints -= 5;
 
-  return totalPoints
-}
+  return totalPoints;
+};
 
 const pointMessages = totalPoints => {
-  if (totalPoints >= 90) return 'Wow! Best Friends Forever!'
-  else if (totalPoints >= 80) return 'Besties'
-  else if (totalPoints >= 70) return 'Matching outfits time!'
-  else if (totalPoints >= 60) return 'I may not know your phone number but I remember your name!'
-  else if (totalPoints >= 50) return 'Fine Friends'
-  else if (totalPoints >= 40) return 'Maybe if you talked less, I would like you more..'
-  else if (totalPoints >= 30) return 'Totally Friendzoned'
-  else if (totalPoints >= 20) return 'Do I know you?'
-  else if (totalPoints >= 10) return 'Wow.. just no.'
-  else return 'Worst match EVER!'
-}
+  if (totalPoints >= 90) return "Wow! Best Friends Forever!";
+  else if (totalPoints >= 80) return "Besties";
+  else if (totalPoints >= 70) return "Matching outfits time!";
+  else if (totalPoints >= 60)
+    return "I may not know your phone number but I remember your name!";
+  else if (totalPoints >= 50) return "Fine Friends";
+  else if (totalPoints >= 40)
+    return "Maybe if you talked less, I would like you more..";
+  else if (totalPoints >= 30) return "Totally Friendzoned";
+  else if (totalPoints >= 20) return "Do I know you?";
+  else if (totalPoints >= 10) return "Wow.. just no.";
+  else return "Worst match EVER!";
+};
 
 const combineStats = (hp, got) => {
-  const hpStats = calculateCharacterPoints(hp)
-  const gotStats = calculateCharacterPoints(got)
-  const totalValue = hpStats + gotStats
+  const hpStats = calculateCharacterPoints(hp);
+  const gotStats = calculateCharacterPoints(got);
+  const totalValue = hpStats + gotStats;
 
-  return totalValue
-}
+  return totalValue;
+};
 
 const StatsMatch = props => {
   if (!props.hp.name || !props.got.name) {
-    return null
+    return null;
   }
-  const value = combineStats(props.hp, props.got)
+  const value = combineStats(props.hp, props.got);
   return (
     <div className='affinityWrapper'>
-      <HeartAnimation totalPoints={value} />
+      <div className='heartWrapper'>
+        <HeartAnimation totalPoints={value} />
+          <h2>{value}%</h2>
+      </div>
       <div className='message'>
         <h3>{pointMessages(value)}</h3>
       </div>
-      <div className='affinity'>
-        <h2>{value}%</h2>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatsMatch
+export default StatsMatch;
