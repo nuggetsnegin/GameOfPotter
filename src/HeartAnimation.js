90335563587bc3456a32737ef85d7924f0ffce8e
Negin <nuggetnegin@gmail.com>
2019-12-09 21:04:51 -0500

    added heartbeat animation based off the percentage of affinity

--- src/HeartAnimation.js
+++ src/HeartAnimation.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react'
 import heart from './assets/heart.png'
 
 class HeartAnimation extends Component {
-    render() {
+  heartBeatCalculator = heartBeat => {
+    if (heartBeat >= 90) return 0.5
+    else if (heartBeat >= 80) return 0.75
+    else if (heartBeat >= 70) return 0.85
+    else if (heartBeat >= 60) return 0.95
+    else if (heartBeat >= 50) return 1
+    else if (heartBeat >= 40) return 1.5
+    else if (heartBeat >= 30) return 1.75
+    else if (heartBeat >= 20) return 2
+    else if (heartBeat >= 10) return 5
+    else return 10
+  }
 
-        console.log(this.props.totalPoints);
-        return (
-            <img className='heartIcon' src={heart} alt='Icon of a red heart' style={{
-                WebkitAnimation: "heartbeat 1.5s ease-in-out infinite both",
-                animation: "heartbeat 1.5s ease-in-out infinite both"
-            }}/>
-        )
-    }
+  render () {
+    const heartBeat = this.props.totalPoints
+
+    const heartBeatAnimation = this.heartBeatCalculator(heartBeat);
+    return (
+      <img
+        className='heartIcon'
+        src={heart}
+        alt='Icon of a red heart'
+        style={{
+          WebkitAnimation: `heartbeat ${heartBeatAnimation}s ease-in-out infinite both`,
+          animation: 'heartbeat 1.5s ease-in-out infinite both'
+        }}
+      />
+    )
+  }
 }
-
-// .heartbeat {
-//     -webkit-animation: heartbeat 1.5s ease-in-out infinite both;
-//     animation: heartbeat 1.5s ease-in-out infinite both;
-//   }
 
 export default HeartAnimation
--- src/StatsMatch.js
+++ src/StatsMatch.js
@@ -11,7 +11,9 @@ const calculateCharacterPoints = character => {
   if (house === 'Hufflepuff' || house === 'House Tyrell') {
     totalPoints += Math.floor(Math.random() * 10) + 11
   }
-  if (house === 'Ravenclaw') totalPoints += Math.floor(Math.random() * 5) + 10
+  if (house === 'Ravenclaw') {
+    totalPoints += Math.floor(Math.random() * 5) + 10
+  }
   if (house === 'House Greyjoy') {
     totalPoints -= Math.floor(Math.random() * 5) + 1
   }
@@ -32,8 +34,10 @@ const pointMessages = totalPoints => {
   if (totalPoints >= 90) return 'Wow! Best Friends Forever!'
   else if (totalPoints >= 80) return 'Besties'
   else if (totalPoints >= 70) return 'Matching outfits time!'
-  else if (totalPoints >= 60) { return 'I may not know your phone number but I remember your name!' } else if (totalPoints >= 50) return 'Fine Friends'
-  else if (totalPoints >= 40) { return 'Maybe if you talked less, I would like you more..' } else if (totalPoints >= 30) return 'Totally Friendzoned'
+  else if (totalPoints >= 60)  return 'I may not know your phone number but I remember your name!'
+  else if (totalPoints >= 50) return 'Fine Friends'
+  else if (totalPoints >= 40)  return 'Maybe if you talked less, I would like you more..' 
+  else if (totalPoints >= 30) return 'Totally Friendzoned'
   else if (totalPoints >= 20) return 'Do I know you?'
   else if (totalPoints >= 10) return 'Wow.. just no.'
   else return 'Worst match EVER!'
@@ -55,7 +59,7 @@ const StatsMatch = props => {
   const value = combineStats(props.hp, props.got)
   return (
     <div className='affinityWrapper'>
-      <HeartAnimation totalPoints={value}/>
+      <HeartAnimation totalPoints={value} />
       <div className='message'>
         <h3>{pointMessages(value)}</h3>
       </div>
